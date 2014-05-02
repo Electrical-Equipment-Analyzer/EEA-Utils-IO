bc13018a152037a34c727e69c5c64a4d0348829c
102m05008 <102m05008@stud.sju.edu.tw>
2014-05-02 07:21:35 +0000

    javadoc

--- src/main/java/tw/edu/sju/ee/eea/util/iepe/IEPEDevice.java
+++ src/main/java/tw/edu/sju/ee/eea/util/iepe/IEPEDevice.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 Leo
+ * Copyright (C) 2014, St. John's University and/or its affiliates. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -18,23 +18,69 @@
 package tw.edu.sju.ee.eea.util.iepe;
 
 /**
+ * A {@code IEPEDevice} is a IEPE sensor input device.
+ * This is the basic mathod to control device.
  *
  * @author Leo
+ * @see IEPEInput
+ * @since 1.0
  */
 public interface IEPEDevice {
 
+    /**
+     * Open this device to connected.
+     * If the device is already opened then invoking this
+     * method has no effect.
+     *
+     * @throws IEPEException if an error occurs
+     */
     public void openDevice() throws IEPEException;
 
+    /**
+     * Close this device to releases resources associated with it.
+     * If the device is already opened then invoking this
+     * method has no effect.
+     *
+     * @throws IEPEException if an error occurs
+     */
     public void closeDevice() throws IEPEException;
 
+    /**
+     * Gets the <code>DeviceID</code>.
+     *
+     * @return the DeviceID.
+     * @throws IEPEException if an error occurs
+     */
     public int getDeviceId() throws IEPEException;
 
+    /**
+     * Configure the setting to device.
+     *
+     * @throws IEPEException if an error occurs
+     */
     public void configure() throws IEPEException;
 
+    /**
+     * Read data from device.
+     *
+     * @param length the sample per channel.
+     * @return Channels data.
+     * @throws IEPEException if an error occurs
+     */
     public double[][] read(int length) throws IEPEException;
 
+    /**
+     * Start the device to acquire data.
+     * 
+     * @throws IEPEException if an error occurs
+     */
     public void start() throws IEPEException;
 
+    /**
+     * Stop the device to acquire data.
+     * 
+     * @throws IEPEException if an error occurs
+     */
     public void stop() throws IEPEException;
 
 }
